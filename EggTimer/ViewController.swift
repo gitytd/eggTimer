4105e035e70a770246e847f12e17407eec649097
gitytd <demiryahyatayyip@gmail.com>
2023-05-20 20:51:34 +0300

    progress bar and alarm sound added.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,22 +1,48 @@
 
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    var counter = 0
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    @IBOutlet weak var textField: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var player: AVAudioPlayer?
+    var timePassed = 0
+    var totalTime = 0
+    var timer = Timer()
+    let eggTimes = ["Soft": 10, "Medium": 420, "Hard": 720]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
         let hardness = sender.currentTitle!
-        counter = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     @objc func updateCounter() {
-        if counter > 0 {
-            print(counter)
-            counter -= 1
+        if timePassed < totalTime {
+            timePassed += 1
+            let progress = Float(timePassed) / Float(totalTime)
+            progressBar.progress = progress
+        } else {
+            textField.text = "HAZIR"
+            playSound()
+            timer.invalidate()
+        }
+    }
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "alarm_sound", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
         }
     }
 }
